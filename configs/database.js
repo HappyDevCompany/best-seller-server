6409b5b2abffc4113bbbfdb54a7d2800b6f917dc
happyDev <93234062+HappyDevCompany@users.noreply.github.com>
2022-11-07 13:15:21 +0100

    update database connection

--- configs/database.js
+++ configs/database.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-const mongoUri = `mongodb://127.0.0.1:27017/best_seller`;
+//const mongoUri = `mongodb://127.0.0.1:27017/best_seller`;
+const mongoUri = `mongodb+srv://happynguala:darknet@cluster0.gzl4p.gcp.mongodb.net/bestseller?retryWrites=true&w=majority`;
 
 mongoose.connect(mongoUri, {
   useNewUrlParser: true,
